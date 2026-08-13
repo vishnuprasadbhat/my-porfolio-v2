e7172cc82051e0c53001afa6c68db97ee0cf5cff
Vishnuprasad Bhat <vishnuprasad_bhat@epam.com>
2026-08-13 11:27:30 +0530

    refactor: simplify TechStack component by removing unused state and effect

--- components/TechStack/index.js
+++ components/TechStack/index.js
@@ -1,19 +1,14 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useTheme } from "next-themes";
 import StackIcon from "tech-stack-icons";
 
 const TechStack = ({ name, icon }) => {
   const { theme } = useTheme();
-  const [mounted, setMounted] = useState();
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
   return (
     <div
       className={`grid grid-cols-1 justify-items-center gap-4 w-full p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
-        mounted && theme === "dark" ? "hover:bg-slate-800" : "hover:bg-slate-50"
+        theme === "dark" ? "hover:bg-slate-800" : "hover:bg-slate-50"
       } hover:scale-105 link`}
     >
       <StackIcon name={icon} className="w-12" />
